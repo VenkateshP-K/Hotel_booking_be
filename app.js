df9a22896b3f7f12bd82135859874a62e504d165
Venkatesh <venkateshpk06@gmail.com>
2024-06-11 21:31:39 +0530

    commit

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ const app = express();
 //use cors
 app.use(cors(
     {
-        origin: 'http://localhost:5173',
+        origin: 'https://hotel-booking-be-hxbx.onrender.com',
         credentials: true
     }
 ));
