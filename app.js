f5a9db2dcd7962aa362d038b167e7f3537812089
Venkatesh <venkateshpk06@gmail.com>
2024-06-24 17:49:34 +0530

    commit

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const roomRouter = require("./routes/roomRoutes");
 const app = express();
 
 app.use(cors({
-    origin: ['http://localhost:5173', 'https://unique-cheesecake-de7813.netlify.app'],
+    origin: ['http://localhost:5173','https://polite-entremet-2d720f.netlify.app/'],
     credentials: true
 }));
 
