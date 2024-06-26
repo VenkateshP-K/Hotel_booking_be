e8fdcaa7ec7f1de9c1a9ef04cb914f211706c948
Venkatesh <venkateshpk06@gmail.com>
2024-06-26 20:08:09 +0530

    commit

--- auth.js
+++ auth.js
@@ -7,6 +7,7 @@ const Auth = {
 
     isAuth: async (req, res, next) => {
         const token = req.cookies.token || req.headers.authorization?.split(' ')[1];
+        console.log('Received token:', token);
         if (!token) {
             return res.status(401).json({ message: "Unauthorized" });
         }
