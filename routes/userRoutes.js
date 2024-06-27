6085b42d0c7c8f08984d98a346d886ed0b99ca17
Venkatesh <venkateshpk06@gmail.com>
2024-06-27 21:23:19 +0530

    commit

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -17,7 +17,7 @@ userRouter.post('/login', userController.login);
 userRouter.get('/me', Auth.isAuth , userController.getMe);
 userRouter.get('/bookedRooms', Auth.isAuth , userController.getMyBookings);
 userRouter.get('/', Auth.isAuth ,Auth.isAdmin, userController.getAllUsers);
-userRouter.put('/update', Auth.isAuth , userController.updateMe);
+userRouter.put('/update/:userId', Auth.isAuth , userController.updateMe);
 userRouter.post('/logout', Auth.isAuth , userController.logout);
 
 //admin routes
