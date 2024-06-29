a47207d4278de0a51c903b603955f1106f29ce9f
Venkatesh <venkateshpk06@gmail.com>
2024-06-29 17:14:48 +0530

    commit

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const roomRouter = require("./routes/roomRoutes");
 const app = express();
 
 const corsOptions = {
-    origin: ['http://localhost:5173'],
+    origin: ['https://localhost:5173',],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With'],
