078c8dea0a4ab4c2ba9519f770689c98307f69f6
Venkatesh <venkateshpk06@gmail.com>
2024-06-29 19:53:33 +0530

    commit

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const roomRouter = require("./routes/roomRoutes");
 const app = express();
 
 const corsOptions = {
-    origin: ['http://localhost:5173','https://timely-starship-086d8d.netlify.app'],
+    origin: ['http://localhost:5173','https://hotelbooking-demo.netlify.app/'],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With'],
